11e50029954b16d2b0ad6e8a1c07ae430fa968fe
TBGSection <103632520+TBGSection@users.noreply.github.com>
2023-04-27 20:52:08 +0800

    Update main.py

--- DLNLP3/main.py
+++ DLNLP3/main.py
@@ -1,7 +1,3 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 import jieba
 import re
 import random
@@ -51,8 +47,6 @@ def Remove(txt):
 
     return txt
 
-
-# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
 
     with open('Data/inf.txt', "r", encoding="ANSI") as file:  # 读入目录
